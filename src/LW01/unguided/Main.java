e5732f40eb8add3936b46887ca05c24606e9e9a5
Navita Fitrianti Refani <navy.brv11@gmail.com>
2026-09-23 18:04:28 +0700

    Labwork 01 - Unguided

--- src/LW01/unguided/Main.java
+++ src/LW01/unguided/Main.java
@@ -1,13 +1,38 @@
-import java.io.file;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(Main.class.getResourceAsStream("washes.txt"));
-        int a = sc.nextInt();
+        WashService[] washes;
+        int[] unitsOf;
 
-        String[][] WashService = new String [a];
+        try (Scanner scanner = new Scanner(new File("washes.txt"))) {
+            int total = scanner.nextInt();
+            washes = new WashService[total];
+            unitsOf = new int[total];
 
+                        for (int i = 0; i < total; i++) {
+                String type = scanner.next();
+                String id = scanner.next();
+                int days = scanner.nextInt();
+                int units = scanner.nextInt();
+
+                if (type.equals("MOTORCYCLE")) {
+                    washes[i] = new MotorcycleWash(id, days);
+                } else {
+                    washes[i] = new CarWash(id, days);
+                }
+                unitsOf[i] = units;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File washes.txt not found.");
+            return;
+        }
+
+           for (int i = 0; i < washes.length; i++) {
+            WashService wash = washes[i];
+            System.out.println(wash.getId() + " | " + wash.label() + " | " + wash.calculateCharge(unitsOf[i]));
+        }
     }
 }
